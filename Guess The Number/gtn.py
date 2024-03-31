8c9bfb22187ef4ba22ee45de6a398fae76a8f695
pavankrishna3 <113927011+pavankrishna3@users.noreply.github.com>
2024-03-31 15:32:45 +0530

    test

---
+++ Guess The Number/gtn.py
@@ -0,0 +1,29 @@
+import random
+
+print("Do you want to guess the number i  am thinking of?")
+ans = input("Yes or No: ")
+
+if ans == "yes"or"yes":
+    
+    print("what is the upper limit?")
+    uplimit=input("Enter an integer :")
+
+    if uplimit.isdigit():
+        uplimit=int(uplimit)
+        if uplimit <= 0:
+            print("select a number  greater than zero.")
+        else:
+            rand = random.randint(1,uplimit)
+    else:
+        print("please enter a number.")
+else:
+    quit()
+
+guesses=0
+while True:
+    guesses += 1
+    num = int(input("Enter your guess :"))
+    if rand == num:
+        print(f"You take {guesses} attempts to guess the number")
+        break
+
