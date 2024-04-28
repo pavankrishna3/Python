057bbd4648fa0f47d8f91d54dfb1c4d2902ec675
pavankrishna3 <113927011+pavankrishna3@users.noreply.github.com>
2024-04-28 13:49:30 +0530

    upadte to file sorter

--- file_sorter.py
+++ file_sorter.py
@@ -1,44 +1,48 @@
 import os
 import shutil
 
-directory  = r"C:\Users\pavan\OneDrive\Desktop\test"
+def sorter():
 
-image = (".jpg", ".png", ".gif", "jpeg",".svg",".webp")
+    directory  = r"C:\Users\pavan\OneDrive\Desktop\test"
 
-video = (".mp4",".mov",".avi",".mkv","webm")
+    image = (".jpg", ".png", ".gif", "jpeg",".svg",".webp")
 
-docs = (".pdf",".docx",".doc")
+    video = (".mp4",".mov",".avi",".mkv","webm")
 
-list = os.listdir(directory)
+    docs = (".pdf",".docx",".doc")
 
-for i in list:
-    
-    if i.lower().endswith(image):
-        img_path =  directory +"\Images"
-        if os.path.exists(img_path):
-            source = os.path.join(directory,i)
-            destination = os.path.join(img_path,i)  
-            shutil.move(source,destination)
-        else:
-            os.mkdir(img_path)
-    if i.lower().endswith(video):
-        vid_path = directory +"\Videos"
-        if os.path.exists(vid_path):
-            source = os.path.join(directory,i)
-            destination = os.path.join(vid_path,i)  
-            shutil.move(source,destination)
-        else:
-            os.mkdir(vid_path)
-    if i.lower().endswith(docs):
-        doc_path = directory +"\Docs"
-        if os.path.exists(doc_path):
-            source = os.path.join(directory,i)
-            destination = os.path.join(doc_path,i)  
-            shutil.move(source,destination)
-        else:
-            os.mkdir(doc_path)
+    list = os.listdir(directory)
+
+    for i in list:
+        
+        if i.lower().endswith(image):
+            img_path =  directory +"\Images"
+            if os.path.exists(img_path):
+                source = os.path.join(directory,i)
+                destination = os.path.join(img_path,i)  
+                shutil.move(source,destination)
+            else:
+                os.mkdir(img_path)
+        if i.lower().endswith(video):
+            vid_path = directory +"\Videos"
+            if os.path.exists(vid_path):
+                source = os.path.join(directory,i)
+                destination = os.path.join(vid_path,i)  
+                shutil.move(source,destination)
+            else:
+                os.mkdir(vid_path)
+        if i.lower().endswith(docs):
+            doc_path = directory +"\Docs"
+            if os.path.exists(doc_path):
+                source = os.path.join(directory,i)
+                destination = os.path.join(doc_path,i)  
+                shutil.move(source,destination)
+            else:
+                os.mkdir(doc_path)
    
-
+if __name__ == "__main__":
+    while 1:
+        sorter()
     
 
 
